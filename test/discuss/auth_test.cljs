587a5061381dbfa778a9591a5c32ae4571ace555
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-04-07 10:44:08 +0200

    Create test-runner and first tests

---
+++ test/discuss/auth_test.cljs
@@ -0,0 +1,7 @@
+(ns ^:figwheel-load discuss.auth-test
+  (:require [cljs.test :refer-macros [deftest is]]
+    ;[cljs-test-example.core :refer [do-something-x-y]]
+            ))
+
+(deftest test-do-something-x-y
+  (is (= 1 3)))
--- test/discuss/test.cljs
+++ test/discuss/test.cljs
@@ -1,13 +1,10 @@
-(ns discuss.test
-  (:require [cljs.test :refer-macros [deftest is run-all-tests]]))
+(ns ^:figwheel-always discuss.test
+  (:require [cljs.test :refer-macros [deftest is run-all-tests]]
+            [discuss.auth-test]))
 
 (enable-console-print!)
 
-(deftest do-i-work
-  (is (= 1 2)))
-
-(deftest fail
-  (is (= "foo" "bar")))
-
 (defn ^:export run []
-  (run-all-tests #"discuss.test"))
+  (print "\n\n##### Running tests ######")
+  (run-all-tests #"discuss.*-test")
+  (println "##### End Running tests ######\n\n"))
