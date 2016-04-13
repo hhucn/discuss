a30e6e67e970099271647026becd6d9998c3cb1b
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-04-13 10:55:04 +0200

    Add more extensions

--- src/discuss/extensions.cljs
+++ src/discuss/extensions.cljs
@@ -2,12 +2,59 @@
 
 (enable-console-print!)
 
-(when (js* "typeof NodeList != \"undefined\"")
-  (extend-type js/NodeList
-    ISeqable
-    (-seq [array] (array-seq array 0))))
-
 (extend-type js/HTMLCollection
+  ISeqable
+  (-seq [array] (array-seq array 0))
+
+  ICounted
+  (-count [a] (alength a))
+
+  IIndexed
+  (-nth
+    ([array n]
+     (if (< n (alength array)) (aget array n)))
+    ([array n not-found]
+     (if (< n (alength array)) (aget array n)
+                               not-found)))
+  ILookup
+  (-lookup
+    ([array k]
+     (aget array k))
+    ([array k not-found]
+     (-nth array k not-found)))
+  IReduce
+  (-reduce
+    ([array f]
+     (ci-reduce array f))
+    ([array f start]
+     (ci-reduce array f start))))
+
+(extend-type js/NodeList
+  ISeqable
+  (-seq [array] (array-seq array 0))
+  ICounted
+  (-count [a] (alength a))
+  IIndexed
+  (-nth
+    ([array n]
+     (if (< n (alength array)) (aget array n)))
+    ([array n not-found]
+     (if (< n (alength array)) (aget array n)
+                               not-found)))
+  ILookup
+  (-lookup
+    ([array k]
+     (aget array k))
+    ([array k not-found]
+     (-nth array k not-found)))
+  IReduce
+  (-reduce
+    ([array f]
+     (ci-reduce array f))
+    ([array f start]
+     (ci-reduce array f start))))
+
+(extend-type js/HTMLDocument
   ISeqable
   (-seq [array] (array-seq array 0)))
 
