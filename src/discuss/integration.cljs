5e67f957746990252deeeb4a48e367e0edae6e12
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-04-14 11:00:37 +0200

    Add fns to process provided references and highlight them

--- src/discuss/integration.cljs
+++ src/discuss/integration.cljs
@@ -1,13 +1,13 @@
 (ns discuss.integration
   "Listen for mouse clicks, get references and highlight them in the article."
   (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require
-    [goog.dom :as gdom]
-    [goog.events :as events]
-    [cljs.core.async :refer [put! chan <!]]
-    [clojure.string :as string]
-    [discuss.lib :as lib]
-    [discuss.extensions])
+  (:require [goog.dom :as gdom]
+            [goog.events :as events]
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [put! chan <!]]
+            [clojure.string :as string]
+            [discuss.lib :as lib]
+            [discuss.extensions])
   (:import [goog.dom]))
 
 (defn get-selection
@@ -42,22 +42,44 @@
 ;;; Integrate references and highlight them in the article
 ;; So I have to iterate through all DOM elements...?
 ;; http://stackoverflow.com/questions/4256339/javascript-how-to-loop-through-all-dom-elements-on-a-page
-(defn convert-reference [ref]
-  (let [body js/document.body.innerHTML
-        ;tspan (str "<span class='arguments'>" ref "</span>")
-        span (gdom/createElement "span")
-        tspan "foo"
-        regex-ref (re-pattern ref)
-        nbody (string/replace body regex-ref (.-innerHTML span))]
-    ;(gdom/setTextContent span ref)
-    (println "Convert...")
-    ;(set! (.. js/document -body -innerHTML) nbody)
+(defn minify-doms
+  "Removes dom-elements, which can never be used as a reference."
+  [doms]
+  (remove #(or (= "SCRIPT" (.-nodeName %))
+               (= "path"   (.-nodeName %))
+               (= "svg"    (.-nodeName %))
+               (= "circle" (.-nodeName %))
+               (= "HTML"   (.-nodeName %))
+               (= "HEAD"   (.-nodeName %))
+               (= "META"   (.-nodeName %))
+               (= "BUTTON" (.-nodeName %)))
+          doms))
 
-    ))
+(defn get-parent
+  "Assuming that the last occurence of my reference is the closest parent of it,
+   this function will return it."
+  [doms ref]
+  (last
+    (filter
+      identity
+      (map (fn [dom]
+             (when (lib/substring? ref (.-innerHTML dom))
+                       dom))
+           doms))))
+
+(defn convert-reference [ref]
+  (let [doms-raw  (.getElementsByTagName js/document "*")
+        doms      (minify-doms doms-raw)
+        parent    (get-parent doms ref)
+        dom-parts (string/split (.-innerHTML parent) (re-pattern ref))
+        dom-ref   (str "<span class='arguments'>" ref "</span><span class='arguments-toggle'></span>")
+        new-dom   (str (first dom-parts) dom-ref (last dom-parts))]
+    (when parent
+      (set! (.-innerHTML parent) new-dom))))
 
 (defn process-references
   "Receives references through the API and prepares them for the next steps."
   [refs]
   (println "Received" (count refs) "references")
   (println refs)
-  (convert-reference "Debug"))
+  (doall (map #(convert-reference %) refs)))
