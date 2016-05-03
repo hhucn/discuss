f8d7a49c3ef9e0a78328750297eefe37ee7960eb
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-05-03 14:52:52 +0200

    Rewrite runner to automatically execute all unit tests

--- resources/test/phantom/runner.js
+++ resources/test/phantom/runner.js
@@ -1,31 +1,53 @@
-var page = require('webpage').create();
 var system = require('system');
+var url, args;
 
-if (system.args.length !== 2) {
-    console.log('Expected a target URL parameter.');
-    phantom.exit(1);
+if (phantom.version.major > 1) {
+    args = system.args;
+    if (args.length < 2) {
+        system.stderr.write('Expected a target URL parameter.');
+        phantom.exit(1);
+    }
+    url = args[1];
+} else {
+    args = phantom.args;
+    if (args.length < 1) {
+        system.stderr.write('Expected a target URL parameter.');
+        phantom.exit(1);
+    }
+    url = args[0];
 }
 
+var page = require('webpage').create();
+
 page.onConsoleMessage = function (message) {
+    // console.log("Test console: " + message);
     console.log(message);
 };
 
-var url = system.args[1];
+console.log("Loading URL: " + url);
 
 page.open(url, function (status) {
-    if (status !== "success") {
+    if (status != "success") {
         console.log('Failed to open ' + url);
-        setTimeout(function() { // https://github.com/ariya/phantomjs/issues/12697
-            phantom.exit(1);
-        }, 0);
+        phantom.exit(1);
     }
 
-    page.evaluate(function() {
-        //OUR TESTS WILL BE EXECUTED HERE!!!
-        discuss.test.run();
+    console.log("Running test.");
+
+    var result = page.evaluate(function() {
+        return discuss.tests.run();
     });
 
-    setTimeout(function() { // https://github.com/ariya/phantomjs/issues/12697
-        phantom.exit(0);
-    }, 0);
+    // NOTE: PhantomJS 1.4.0 has a bug that prevents the exit codes
+    //        below from being returned properly. :(
+    //
+    // http://code.google.com/p/phantomjs/issues/detail?id=294
+
+    if (result != 0) {
+        console.log("*** Test failed! ***");
+        phantom.exit(1);
+    }
+
+    console.log("Test succeeded.");
+    phantom.exit(0);
 });
