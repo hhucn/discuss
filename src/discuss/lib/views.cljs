9beb420a80c868da020e2602d23c980255541c55
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-05-06 10:51:58 +0200

    Create separate namespace for multiple libs

---
+++ src/discuss/lib/views.cljs
@@ -0,0 +1,38 @@
+(ns discuss.lib.views
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]))
+
+(defn fa-icon
+  "Wrapper for font-awesome icons."
+  ([class]
+   (dom/i #js {:className (str "fa " class)}))
+  ([class f]
+   (dom/i #js {:className (str "pointer fa " class)
+               :onClick   f})))
+
+(defn logo
+  "If no function is provided, show logo as is. Else bind function to onClick-event and add
+   pointer-class."
+  ([]
+   (fa-icon "fa-comments"))
+  ([f]
+   (fa-icon "fa-comments" f)))
+
+(defn safe-html
+  "Creates DOM element with interpreted HTML."
+  [string]
+  (dom/span #js {:dangerouslySetInnerHTML #js {:__html string}}))
+
+(defn commit-target-value
+  "Set local state of view, parse the value of the target of val."
+  [key val owner]
+  (om/set-state! owner key (.. val -target -value)))
+
+(defn display
+  "Toggle display view."
+  [show]
+  (if show
+    #js {}
+    #js {:display "none"}))
+
+(defn toggle-show [show] (if show false true))
