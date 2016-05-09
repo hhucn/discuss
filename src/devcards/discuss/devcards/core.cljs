a7791567c87ab928ae39916941d348f71f9d517a
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-05-09 07:54:33 +0200

    Add first devcards

--- src/devcards/discuss/devcards/core.cljs
+++ src/devcards/discuss/devcards/core.cljs
@@ -1,5 +1,23 @@
 (ns discuss.devcards.core
-  (:require [devcards.core :as dc :refer-macros [defcard deftest]]))
+  (:require [devcards.core :as dc :refer-macros [defcard deftest]]
+            [cljs.test :refer-macros [is async]]
+            [om.dom :as dom]
+            [discuss.extensions]
+            [discuss.debug :as debug]
+            [discuss.lib :as lib]
+            [discuss.views :as views]))
 
-(defcard foo
-         "# hello world")
+(enable-console-print!)
+
+(defcard main
+         "## Main discuss Component"
+         (dc/om-root views/main-view) lib/app-state)
+
+(defcard debug
+         (dc/om-root debug/debug-view) lib/app-state)
+
+(defcard global-state
+         @lib/app-state)
+
+(deftest bar
+         "baz")
