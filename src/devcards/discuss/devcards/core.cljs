3af1edccab850c4ba3c13ccea0e02bbe035c3115
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-05-09 08:30:13 +0200

    Update cards

--- src/devcards/discuss/devcards/core.cljs
+++ src/devcards/discuss/devcards/core.cljs
@@ -2,19 +2,29 @@
   (:require [devcards.core :as dc :refer-macros [defcard deftest]]
             [cljs.test :refer-macros [is async]]
             [om.dom :as dom]
+            [discuss.core :as core]
             [discuss.extensions]
-            [discuss.debug :as debug]
             [discuss.lib :as lib]
             [discuss.views :as views]))
 
 (enable-console-print!)
 
 (defcard main
-         "## Main discuss Component"
-         (dc/om-root views/main-view) lib/app-state)
+         "#### Main discuss Component"
+         (dc/om-root views/main-view)
+         lib/app-state
+         {:history true})
 
-(defcard debug
-         (dc/om-root debug/debug-view) lib/app-state)
+
+(defcard control-buttons
+         ""
+         (dom/button #js {:className "btn btn-info"
+                          :onClick   core/main}
+                     (dom/i #js {:className "fa fa-fort-awesome"})))
+
+#_(defcard debug
+         (dc/om-root debug/debug-view)
+         lib/app-state)
 
 (defcard global-state
          @lib/app-state)
