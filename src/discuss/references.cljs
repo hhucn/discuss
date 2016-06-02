c48164c52da60ae13e8ee074ee8e1c97d300198d
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-06-02 16:54:46 +0200

    Prepare ajax handler when receiving ajax response

--- src/discuss/references.cljs
+++ src/discuss/references.cljs
@@ -3,13 +3,21 @@
    or providing a form to use it in the own statement."
   (:require [om.core :as om]
             [om.dom :as dom]
-            [discuss.communication :as com]))
+            [discuss.communication :as com]
+            [discuss.config :as config]))
+
+(defn reference-usage-handler
+  "Handler to process information about the reference."
+  [response]
+  ;; TODO write common wrapper for handlers
+  )
 
 (defn show-usages
   "Show usages of the provided reference.
    @TODO"
   [reference]
-  (comment "Make AJAX call via API to get some information about the reference."))
+  (let [url (str (:base config/api) (get-in config/api [:get :reference-usages]) "/" reference)]
+    (com/ajax-get url {} reference-usage-handler)))
 
 (defn dialog
   "Show a dialog to give the user the option to choose, whether she wants to get some information about the statement
