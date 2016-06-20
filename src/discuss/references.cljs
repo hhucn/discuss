7b90a0c1a4b5395b68f875092832007b8bc67abf
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-06-20 16:36:57 +0200

    Add view showing all usage; add separate usage-view; private fn

--- src/discuss/references.cljs
+++ src/discuss/references.cljs
@@ -8,17 +8,23 @@
             [discuss.sidebar :as sidebar]
             [discuss.utils.common :as lib]))
 
+(defn- get-reference-usages-from-app-state
+  "Return list of reference usages, which were previously stored in the app-state.
+   TODO: optimize"
+  []
+  (get-in @lib/app-state [:common :reference-usages]))
+
 (defn reference-usage-handler
-  "Handler to process information about the reference."
+  "Handler to process information about the reference. Store results and change view."
   [response]
   (let [res (com/success-handler response)]
-    (println res)))
+    (lib/update-state-item! :common :reference-usages (fn [_] res))
+    (lib/change-view! :reference-usages)))
 
 (defn query-reference-details
   "Show usages of the provided reference."
   [reference-id]
   (let [url (str (:base config/api) (get-in config/api [:get :reference-usages]) "/" reference-id)]
-    (println "OMG I am here!")
     (com/ajax-get url {} reference-usage-handler)))
 
 
@@ -27,7 +33,6 @@
   "When clicking on a reference directly in the text, interact with the user to give her the choice of what
    her next steps might be."
   [reference]
-  #_(com/ajax-get (:url reference))
   (lib/change-view! :reference-dialog)
   (lib/save-selected-reference! reference)
   (sidebar/show)
@@ -49,9 +54,29 @@
                (dom/button #js {:className "btn btn-primary"}
                            "Create new Statement with this reference")))))
 
+(defn usage-view
+  "List single usages of reference."
+  [data _owner]
+  (reify om/IRender
+    (render [_]
+      (let [reference (:reference data)
+            issue (:issue data)
+            statement (:statement data)
+            author (:author data)]
+        (dom/div #js {:className "bs-callout bs-callout-info"}
+                 (dom/a #js {:href    "javascript:void(0)"
+                             :onClick #(com/ajax-get (:url statement))}
+                        (dom/strong nil (:text statement)))
+                 (dom/div nil "Issue: " (:title issue))
+                 (dom/div nil "Author: " (:nickname author)))))))
+
 (defn usages-view
   "List with details showing the usages of the given reference."
   []
   (reify om/IRender
     (render [_]
-      (dom/h4 nil "foo"))))
+      (let [usages (get-reference-usages-from-app-state)]
+        (dom/div nil
+                 (dom/h6 nil (str "Received " (count usages) " usage(s)."))
+                 (apply dom/div nil
+                        (map #(om/build usage-view (lib/merge-react-key %)) usages)))))))
