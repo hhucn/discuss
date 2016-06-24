f3f4abdc371d2799aa37929d04cbc0b38bdbdfad
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-06-24 15:18:11 +0200

    Update deps

--- project.clj
+++ project.clj
@@ -7,12 +7,12 @@
   :min-lein-version "2.5.3"
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.36"]
-                 [org.clojure/core.async "0.2.374" :exclusions [org.clojure/tools.reader]]
+                 [org.clojure/clojurescript "1.9.93"]
+                 [org.clojure/core.async "0.2.385" :exclusions [org.clojure/tools.reader]]
                  [org.omcljs/om "1.0.0-alpha36"]
-                 [cljs-ajax "0.5.5"]                        ; AJAX for om
-                 [com.cognitect/transit-cljs "0.8.237"]     ; Better JSON support
-                 [figwheel-sidecar "0.5.3-2" :scope "devcards"]
+                 [cljs-ajax "0.5.8"]                        ; AJAX for om
+                 [com.cognitect/transit-cljs "0.8.239"]     ; Better JSON support
+                 [figwheel-sidecar "0.5.4-4" :scope "devcards"]
                  [devcards "0.2.1-7" :scope "devcards" :exclusions [org.clojure/clojurescript]]]
 
   :plugins [[lein-figwheel "0.5.3-2"]
