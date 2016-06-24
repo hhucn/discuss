6f784d004c49515b3308e3da9edb9ff16503f89c
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-06-24 15:05:22 +0200

    Create lib for common bootstrap elements

---
+++ src/discuss/utils/bootstrap.cljs
@@ -0,0 +1,12 @@
+(ns discuss.utils.bootstrap
+  "Reusable components, which use twitter bootstrap to reduce redundancy."
+  (:require [om.dom :as dom]
+            [discuss.utils.common :as lib]))
+
+(defn button-primary
+  "Create dom element of a bootstrap primary button."
+  [fn & strs]
+  (dom/button #js {:className "btn btn-primary"
+                   :onClick   fn
+                   :react-key (lib/get-unique-key)}
+              strs))
