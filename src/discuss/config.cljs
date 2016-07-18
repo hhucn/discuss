71bf27975b1739f9e04ab6e494aeeac808d647c2
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-07-18 11:09:13 +0200

    Use local address of dabas-

--- src/discuss/config.cljs
+++ src/discuss/config.cljs
@@ -4,7 +4,7 @@
 
 (def user "Q2hyaXN0aWFu")
 
-(def api {:host "http://dbas/"
+(def api {:host "http://localhost:4284/"
           :init "api/elektroautos"
           :base "api/"
           :add  {:add-start-statement "add/start_statement"
