a78b81add94794419a4c43b447f1c63d08db940e
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-08-09 14:57:33 +0200

    Add default buttons

--- src/discuss/utils/bootstrap.cljs
+++ src/discuss/utils/bootstrap.cljs
@@ -3,10 +3,22 @@
   (:require [om.dom :as dom]
             [discuss.utils.common :as lib]))
 
-(defn button-primary
-  "Create dom element of a bootstrap primary button."
-  [fn & strs]
-  (dom/button #js {:className "btn btn-primary"
+(defn button
+  "Create dom element of a bootstrap button."
+  [fn class & strs]
+  (dom/button #js {:className (str "btn " class)
                    :onClick   fn
                    :key       (lib/get-unique-key)}
               strs))
+
+(defn button-primary
+  "Create dom element of a bootstrap primary button."
+  [fn & strs] (button fn "btn-primary" strs))
+
+(defn button-default
+  "Create dom element of a bootstrap default button."
+  [fn & strs] (button fn "btn-default" strs))
+
+(defn button-default-sm
+  "Create dom element of a bootstrap default small button."
+  [fn & strs] (button fn "btn-default btn-sm" strs))
