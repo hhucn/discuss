db06f4b7d23b5345454c1c02218d35e169561c5a
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-08-25 13:40:46 +0200

    Remove ip output

--- docker_init.sh
+++ docker_init.sh
@@ -12,12 +12,5 @@ sass css/discuss.sass css/discuss.css --style compressed > /dev/null 2>&1
 sass css/zeit.sass css/zeit.css --style compressed > /dev/null 2>&1
 rm -rf .sass-cache
 
-# Print IP address
-ip=`ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1`
 port=8888
-
-printf "\n###################################################"
-printf "\n# Connect to discuss via http://$ip:$port"
-printf "\n###################################################\n"
-
 python2 -m SimpleHTTPServer ${port}
