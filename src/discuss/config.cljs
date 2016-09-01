ec98cb8a9ec3806d0423be97a47ea2edc9d3e1ae
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-09-01 18:20:51 +0200

    Reformat config

--- src/discuss/config.cljs
+++ src/discuss/config.cljs
@@ -4,14 +4,14 @@
 
 (def user "Q2hyaXN0aWFu")
 
-(def api {:host "http://localhost:4284/"
-          :init "api/elektroautos"
-          :base "api/"
+(def api {:host  "http://localhost:4284/"
+          :init  "api/elektroautos"
+          :base  "api/"
           :login "api/login"
-          :add  {:add-start-statement "api/add/start_statement"
-                 :add-start-premise   "api/add/start_premise"
-                 :add-justify-premise "api/add/justify_premise"}
-          :get  {:references       "api/get/references"
-                 :reference-usages "api/get/reference/usages"
-                 :statements       "api/get/statements"
-                 :statement-url    "api/get/statement/url"}})
+          :add   {:add-start-statement "api/add/start_statement"
+                  :add-start-premise   "api/add/start_premise"
+                  :add-justify-premise "api/add/justify_premise"}
+          :get   {:references       "api/get/references"
+                  :reference-usages "api/get/reference/usages"
+                  :statements       "api/get/statements"
+                  :statement-url    "api/get/statement/url"}})
