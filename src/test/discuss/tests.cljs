3ffb405efe41e900d62409690d6e522cf3efad57
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-10-04 14:56:26 +0200

    Optimize tests

--- src/test/discuss/tests.cljs
+++ src/test/discuss/tests.cljs
@@ -1,10 +1,12 @@
 (ns discuss.tests
   "Test runner to execute all tests."
   (:require [doo.runner :refer-macros [doo-tests]]
-            [discuss.utils.common-test]
-            [discuss.components.clipboard-test]))
+            [discuss.components.clipboard-test]
+            [discuss.references.lib-test]
+            [discuss.utils.common-test]))
 
 (enable-console-print!)
 
 (doo-tests 'discuss.components.clipboard-test
+           'discuss.references.lib-test
            'discuss.utils.common-test)
