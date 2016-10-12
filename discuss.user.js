527e2994127a85c018a653aae9f15a558a684407
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-10-12 16:31:44 +0200

    Add greasemonkey user script to discuss everywhere!

---
+++ discuss.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         discuss
+// @namespace    discuss
+// @version      0.2.1
+// @description  Embedding dialog-based discussions into the Web
+// @author       Christian Meter
+// @include      *
+// @require      https://christian-meter.de/discuss.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // discuss.core.main();
+    console.log('Successfully loaded discuss.');
+})();
