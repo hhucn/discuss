d27a07300ebe20d6f8e499db604c8bc38ad185d7
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-02-10 15:39:05 +0100

    Squashed commit of the following:
    commit f0c352238b12da3f39e51f94786764ae55f34786
    Author: Christian Meter <christian.meter@uni-duesseldorf.de>
    Date:   Fri Feb 10 15:38:53 2017 +0100
    
        Tidy up; remove dependencies; remove devcards
    
    commit 6490c0d79f1406371124280362e5ba0f2ec1015d
    Author: Christian Meter <christian.meter@uni-duesseldorf.de>
    Date:   Fri Feb 10 15:24:35 2017 +0100
    
        Merge feature/build-image

--- src/devcards/discuss/devcards/test.cljs
+++
@@ -1,9 +0,0 @@
- (ns discuss.devcards.test
-   (:require [discuss.devcards.core]
-             [discuss.devcards.lib]))
-
- (defn -main [& args]
-   (println "Hello world!")
-   (cljs.test/run-all-tests))
-
-(set! *main-cli-fn* -main)
