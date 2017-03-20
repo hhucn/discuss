6b15cae4ef8f13263be40a8205013fd442e6e690
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-03-20 15:39:48 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.495"]
-                 [org.clojure/core.async "0.2.395" :exclusions [org.clojure/tools.reader]]
+                 [org.clojure/core.async "0.3.442" :exclusions [org.clojure/tools.reader]]
                  [org.clojure/test.check "0.9.0"]
                  [org.omcljs/om "1.0.0-alpha47"]
                  [cljs-ajax "0.5.8"]                        ; AJAX for om
@@ -35,7 +35,7 @@
   ;; setting up nREPL for Figwheel and ClojureScript dev
   ;; Please see:
   ;; https://github.com/bhauman/lein-figwheel/wiki/Using-the-Figwheel-REPL-within-NRepl
-  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.1"]
+  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.2"]
                                   [figwheel-sidecar "0.5.9"]
                                   [com.cemerick/piggieback "0.2.1"]]
                    ;; need to add dev source path here to get user.clj loaded
