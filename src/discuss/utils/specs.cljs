ed5fdb5ee5d738b41d9f948d5cc35ba14af36897
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-04-05 15:13:00 +0200

    Fix spec

--- src/discuss/utils/specs.cljs
+++ src/discuss/utils/specs.cljs
@@ -5,13 +5,8 @@
 (s/fdef discuss.utils.common/trim-all
         :args (s/cat :str string?)
         :ret string?
-        :fn #(> (-> % :ret count) (-> % :args :str count)))
+        :fn #(<= (-> % :ret count) (-> % :args :str count)))
 
-(stest/instrument 'discuss.utils.common/trim-all)
+;; (stest/instrument 'discuss.utils.common/trim-all)
 ;; (s/exercise-fn `discuss.utils.common/trim-all)
 ;; (stest/abbrev-result (first (stest/check `discuss.utils.common/trim-all)))
-
-;; (map #(-> % :clojure.test.check/ret pprint/pprint) (stest/check `discuss.utils.common/trim-all))
-
-;; (require '[discuss.utils.common-test])
-;; (discuss.utils.common-test/check' (stest/check `discuss.utils.common/trim-all))
