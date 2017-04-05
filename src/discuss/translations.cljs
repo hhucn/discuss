548e1bcdb860cf01c575520c4dd1891e2027d5fa
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-04-05 16:50:27 +0200

    Reduce whitespace in translations

--- src/discuss/translations.cljs
+++ src/discuss/translations.cljs
@@ -4,65 +4,66 @@
 (def available [[:de "deutsch"]
                 [:en "english"]])
 
-(def translations {:de {:common     {:and              "und"
-                                     :author           "Autor"
-                                     :back             "Zurück"
-                                     :chars-remaining  "Zeichen verbleibend"
-                                     :close            "Schließen"
-                                     :hello            "Hallo"
-                                     :issue            "Diskussionsthema"
-                                     :login            "Login"
-                                     :logout           "Logout"
-                                     :options          "Einstellungen"
-                                     :save             "Speichern"
-                                     :show-discuss     "Zeige discuss"
-                                     :start-discussion "Starte die Diskussion"}
-                        :discussion {:add-argument "Ein neues Argument hinzufügen"
-                                     :current      "Aktuelle Diskussion"
-                                     :restart      "Neustarten"
-                                     :submit       "Abschicken"}
-                        :errors     {:login "Login fehlgeschlagen. Vermutlich sind die Zugangsdaten nicht korrekt."}
-                        :find       {:find-statement "Finde Aussage in Diskussion"}
-                        :login      {:nickname "Benutzername"
-                                     :password "Passwort"}
-                        :nav        {:home "Start"
-                                     :find "Suchen"}
-                        :options    {:heading "Einstellungen"
-                                     :lang    "Interface-Sprache"}
-                        :references {:jump       "Springe in die Diskussion"
-                                     :usages     "In welchen Argumenten wird dieser Textausschnitt verwendet?"
-                                     :where-used "Wo wird diese Referenz verwendet?"
-                                     :find-statement "Finde Aussage in der Diskussion"
-                                     :clipboard  "Ziehe diese Referenzen in das Textfeld beim Erzeugen eines neuen Arguments, um die Referenz zu nutzen."}}
-                   :en {:common     {:and              "and"
-                                     :author           "Author"
-                                     :back             "Back"
-                                     :chars-remaining  "characters remaining"
-                                     :close            "Close"
-                                     :hello            "Hello"
-                                     :issue            "Issue"
-                                     :login            "Login"
-                                     :logout           "Logout"
-                                     :options          "Options"
-                                     :save             "Save"
-                                     :show-discuss     "Show discuss"
-                                     :start-discussion "Start Discussion"}
-                        :discussion {:add-argument "Add a new argument"
-                                     :current      "Current Discussion"
-                                     :restart      "Restart"
-                                     :submit       "Submit"}
-                        :errors     {:login "Could not login. Maybe your credentials are wrong."}
-                        :find       {:find-statement "Find statement in the discussion"}
-                        :login      {:nickname "Nickname"
-                                     :password "Password"}
-                        :nav        {:home "Home"
-                                     :find "Find"}
-                        :options    {:heading "Options"
-                                     :lang    "Language of Interface"}
-                        :references {:jump       "Jump into the discussion"
-                                     :usages     "In which arguments has this reference been used?"
-                                     :where-used "Where has this reference been used?"
-                                     :clipboard  "Drag these references into the input field when creating a new argument."}}})
+(def translations
+  {:de {:common     {:and              "und"
+                     :author           "Autor"
+                     :back             "Zurück"
+                     :chars-remaining  "Zeichen verbleibend"
+                     :close            "Schließen"
+                     :hello            "Hallo"
+                     :issue            "Diskussionsthema"
+                     :login            "Login"
+                     :logout           "Logout"
+                     :options          "Einstellungen"
+                     :save             "Speichern"
+                     :show-discuss     "Zeige discuss"
+                     :start-discussion "Starte die Diskussion"}
+        :discussion {:add-argument "Ein neues Argument hinzufügen"
+                     :current      "Aktuelle Diskussion"
+                     :restart      "Neustarten"
+                     :submit       "Abschicken"}
+        :errors     {:login "Login fehlgeschlagen. Vermutlich sind die Zugangsdaten nicht korrekt."}
+        :find       {:find-statement "Finde Aussage in Diskussion"}
+        :login      {:nickname "Benutzername"
+                     :password "Passwort"}
+        :nav        {:home "Start"
+                     :find "Suchen"}
+        :options    {:heading "Einstellungen"
+                     :lang    "Interface-Sprache"}
+        :references {:jump       "Springe in die Diskussion"
+                     :usages     "In welchen Argumenten wird dieser Textausschnitt verwendet?"
+                     :where-used "Wo wird diese Referenz verwendet?"
+                     :find-statement "Finde Aussage in der Diskussion"
+                     :clipboard  "Ziehe diese Referenzen in das Textfeld beim Erzeugen eines neuen Arguments, um die Referenz zu nutzen."}}
+   :en {:common     {:and              "and"
+                     :author           "Author"
+                     :back             "Back"
+                     :chars-remaining  "characters remaining"
+                     :close            "Close"
+                     :hello            "Hello"
+                     :issue            "Issue"
+                     :login            "Login"
+                     :logout           "Logout"
+                     :options          "Options"
+                     :save             "Save"
+                     :show-discuss     "Show discuss"
+                     :start-discussion "Start Discussion"}
+        :discussion {:add-argument "Add a new argument"
+                     :current      "Current Discussion"
+                     :restart      "Restart"
+                     :submit       "Submit"}
+        :errors     {:login "Could not login. Maybe your credentials are wrong."}
+        :find       {:find-statement "Find statement in the discussion"}
+        :login      {:nickname "Nickname"
+                     :password "Password"}
+        :nav        {:home "Home"
+                     :find "Find"}
+        :options    {:heading "Options"
+                     :lang    "Language of Interface"}
+        :references {:jump       "Jump into the discussion"
+                     :usages     "In which arguments has this reference been used?"
+                     :where-used "Where has this reference been used?"
+                     :clipboard  "Drag these references into the input field when creating a new argument."}}})
 
 (defn- prepend-translation
   "Lookup given key and prepend some string to it."
