16e45bb4ec6fd6ca571260aba170194d7fddcf5d
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-04-10 13:13:31 +0200

    Evaluate lazy test results, Fix #28

--- src/test/discuss/lib.cljs
+++ src/test/discuss/lib.cljs
@@ -3,7 +3,8 @@
             [cljs.test :refer-macros [is]]))
 
 (defn- summarize-results' [spec-check]
-  (map #(-> % :clojure.test.check/ret pprint/pprint) spec-check))
+  (doall (map #(-> % :clojure.test.check/ret pprint/pprint) spec-check)))
 
 (defn check' [spec-check]
-  (is (nil? (-> spec-check first :failure)) (summarize-results' spec-check)))
+  (summarize-results' spec-check)
+  (is (nil? (-> spec-check first :failure))))
