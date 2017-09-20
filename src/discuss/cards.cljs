61af9dbd6c22675f211b210701be13b5bfe2eceb
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-09-20 16:49:55 +0200

    Add sample om-next devcard

--- src/discuss/cards.cljs
+++ src/discuss/cards.cljs
@@ -1,15 +1,28 @@
 (ns discuss.cards
   (:require [om.core :as om :include-macros true]
-            [sablono.core :as sab :include-macros true]
+            [sablono.core :as html :refer-macros [html]]
+            [om.next :as nom :refer-macros [defui]]
+            [discuss.parser :as parser]
             [discuss.utils.common :as lib]
             [discuss.views :as views])
-  (:require-macros [devcards.core :as dc :refer [defcard defcard-om deftest]]))
+  (:require-macros [devcards.core :as dc :refer [defcard defcard-om defcard-om-next deftest]]))
 
 (enable-console-print!)
 
 (defcard-om discuss
   views/main-view
   lib/app-state)
+
+(defui Foo
+  Object
+  (render [this]
+          (html [:h1 "Wuki"])))
+(def foo (nom/factory Foo))
+
+(defcard-om-next wuki-card
+  foo
+  parser/reconciler)
+
 
 ;; -----------------------------------------------------------------------------
 ;; Start devcards
@@ -18,6 +31,6 @@
   ;; conditionally start the app based on whether the #main-app-area
   ;; node is on the page
   (when-let [node (.getElementById js/document "main-app-area")]
-    (.render js/ReactDOM (sab/html [:div "This is working"]) node)))
+    (.render js/ReactDOM (html [:div "This is working"]) node)))
 
 (main)
