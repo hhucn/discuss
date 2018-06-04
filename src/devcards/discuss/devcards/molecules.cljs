1f4c8d93921c6a42cde1866dd23fd3f62eb44955
Christian Meter <christian.meter@uni-duesseldorf.de>
2018-06-04 13:19:54 +0200

    Add defcard with some sample requests

--- src/devcards/discuss/devcards/molecules.cljs
+++ src/devcards/discuss/devcards/molecules.cljs
@@ -1,11 +1,26 @@
 (ns discuss.devcards.molecules
   (:require [devcards.core :as dc :refer-macros [defcard defcard-om-next]]
+            [sablono.core :as html :refer-macros [html]]
             [discuss.parser :as parser]
-            [discuss.views :as views]))
+            [discuss.views :as views]
+            [discuss.communication.lib :as comlib]))
+
+(defcard sample-requests
+  "Do some sample requests with `ajax-get`."
+  (html [:div
+         [:button.btn.btn-sm.btn-primary {:onClick #(comlib/ajax-get "/town-has-to-cut-spending/attitude/36" nil comlib/process-discussion-step)}
+          "/town-has-to-cut-spending/attitude/36"]
+         [:br]
+         [:button.btn.btn-sm.btn-primary {:onClick #(comlib/ajax-get "/cat-or-dog" nil comlib/process-discussion-step)}
+          "/cat-or-dog"]
+         [:br]
+         [:button.btn.btn-sm.btn-primary {:onClick #(comlib/ajax-get "/town-has-to-cut-spending/reaction/47/undercut/48?history=/attitude/38-/justify/38/agree" nil comlib/process-discussion-step)}
+          "/town-has-to-cut-spending/reaction/47/undercut/48?history=/attitude/38-/justify/38/agree"]]))
 
 (defcard-om-next discussion-elements
   views/DiscussionElements
-  parser/reconciler)
+  parser/reconciler
+  {:inspect-data true :history true})
 
 (defcard-om-next view-dispatcher
   views/ViewDispatcher
