5c96dd370c1eefbfe612e659fa717dca4c61f49c
Christian Meter <christian.meter@uni-duesseldorf.de>
2018-07-23 13:59:30 +0200

    Fix tooltips

--- src/discuss/components/tooltip.cljs
+++ src/discuss/components/tooltip.cljs
@@ -78,7 +78,7 @@
     (if (and (pos? (count selection))
              (not= selection (lib/get-selection)))
       (do (move-to-selection)
-          (lib/store-to-app-state! :selection/current))
+          (lib/store-to-app-state! 'selection/current selection))
       (hide))))
 
 (let [clicks (listen (.getElementById js/document "discuss-text") "click")]
