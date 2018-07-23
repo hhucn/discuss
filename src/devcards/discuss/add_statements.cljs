46a5183b8ee12150d05169c5e184a71864dd6536
Christian Meter <christian.meter@uni-duesseldorf.de>
2018-07-23 13:58:32 +0200

    Prepare defcard to add new statements

--- src/devcards/discuss/add_statements.cljs
+++ src/devcards/discuss/add_statements.cljs
@@ -3,19 +3,21 @@
             [discuss.parser :as parser]
             [discuss.views :as views]
             [devcards.discuss.utils :as dutils]
-            [om.next :as om]))
+            [om.next :as om]
+            [discuss.utils.common :as lib]))
 
-(defcard shortcuts dutils/shortcuts)
+(defcard shortcuts
+  dutils/shortcuts)
 
 (def sample-api-route "/town-has-to-cut-spending/justify/37/agree?history=/attitude/37")
-
-(def ^:private stmts-reconciler
-  (om/reconciler {:state (assoc @(om/app-state parser/reconciler) :api/last-call sample-api-route)
-                  :parser (om/parser {:read parser/read :mutate parser/mutate})}))
 
 (defcard-om-next main-content-view
   views/MainContentView
   parser/reconciler)
+
+(dc/defcard-doc
+  "Do request to the following Route:"
+  sample-api-route)
 
 (defcard-om-next add-new-statement
   views/AddElement
