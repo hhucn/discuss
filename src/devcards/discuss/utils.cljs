124c88ae9f92c2514bcbb88afb5a11f186dc2870
Christian Meter <christian.meter@uni-duesseldorf.de>
2018-07-23 13:58:53 +0200

    Add more buttons to devcard-shortcuts

--- src/devcards/discuss/utils.cljs
+++ src/devcards/discuss/utils.cljs
@@ -1,6 +1,9 @@
 (ns devcards.discuss.utils
   (:require [discuss.communication.auth :as auth]
-            [sablono.core :as html :refer-macros [html]]))
+            [sablono.core :as html :refer-macros [html]]
+            [discuss.utils.common :as lib]))
 
 (def shortcuts
-  (html [:div.btn.btn-primary {:onClick #(auth/login "Christian" "iamgroot")} "Login"]))
+  (html [:div
+         [:div.btn.btn-primary {:onClick #(auth/login "Christian" "iamgroot")} "Login"]
+         [:div.btn.btn-primary {:onClick #(lib/last-api! "/town-has-to-cut-spending/justify/37/agree?history=/attitude/37")} "Set :api/last-url"]]))
