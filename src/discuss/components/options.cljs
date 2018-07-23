cc231ac152f9898a0720e73224017e17f9edee4c
Christian Meter <christian.meter@uni-duesseldorf.de>
2018-07-23 15:13:58 +0200

    Remove om.now language components

--- src/discuss/components/options.cljs
+++ src/discuss/components/options.cljs
@@ -1,53 +1,10 @@
 (ns discuss.components.options
-  (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]
-            [om.next :as nom :refer-macros [defui]]
+  (:require [om.next :as om :refer-macros [defui]]
             [sablono.core :as html :refer-macros [html]]
             [discuss.utils.bootstrap :as bs]
             [discuss.translations :as translations :refer [translate]]
             [discuss.utils.common :as lib]
             [discuss.utils.views :as vlib]))
-
-(defn- option-row
-  "Generic row for multiple settings."
-  {:deprecated 0.4}
-  [description content]
-  (dom/div #js {:className "text-center"}
-           (dom/div nil description)
-           (dom/div nil content)))
-
-(defn- language
-  "Component for a single language selection."
-  {:deprecated 0.4}
-  [language]
-  (reify
-    om/IRender
-    (render [_]
-      (dom/span nil
-                (bs/button-default-sm #(lib/language-next! (first language)) (second language))
-                " "))))
-
-(defn- language-row
-  "Language Chooser."
-  {:deprecated 0.4}
-  []
-  (option-row (dom/span nil (vlib/fa-icon "fa-flag") (translate :options :lang :space))
-              (apply dom/div nil
-                     (map #(om/build language %) translations/available))))
-
-(defn view
-  "Main view for options."
-  {:deprecated 0.4}
-  []
-  (reify
-    om/IRender
-    (render [_]
-      (dom/div nil
-               (vlib/view-header (translate :options :heading))
-               (language-row)))))
-
-;; -----------------------------------------------------------------------------
-;; om.next
 
 (defn- language-button
   "Create button to set language."
@@ -60,4 +17,4 @@
           (html [:div (vlib/view-header (translate :options :heading))
                  [:div (vlib/fa-icon "fa-flag") (translate :options :lang :space)]
                  (interpose " " (mapv language-button translations/available))])))
-(def options (nom/factory Options))
+(def options (om/factory Options))
