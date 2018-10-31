26e40669ea4293915079205a75c94ea4f2cbbfdb
Christian Meter <christian.meter@uni-duesseldorf.de>
2018-10-31 23:00:33 +0100

    Remove old code

--- src/discuss/config.cljs
+++ src/discuss/config.cljs
@@ -14,12 +14,6 @@
   "Available log-levels: :severe :warning :info :config :fine :finer :finest."
   :fine)
 
-(defn- get-protocol [url]
-  (first (str/split url #":")))
-
-(defn- make-host [host]
-  (str (get-protocol (.. js/window -location -href)) "://" host))
-
 (def api {:host  remote-host
           :init  "/town-has-to-cut-spending"
           :base  "/"
