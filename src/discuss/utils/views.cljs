1e3594d18cf8900ce6665c494c42a678eed520a7
Christian Meter <christian.meter@uni-duesseldorf.de>
2018-12-03 14:27:53 +0100

    Move helper functions to view-lib

--- src/discuss/utils/views.cljs
+++ src/discuss/utils/views.cljs
@@ -1,9 +1,12 @@
 (ns discuss.utils.views
-  (:require [goog.dom :as gdom]
+  (:require [sablono.core :as html :refer-macros [html]]
+            [goog.dom :as gdom]
             [goog.string :as gstring]
             [om.core :as om]
             [om.dom :as dom]
-            [discuss.utils.common :as lib]))
+            [discuss.utils.common :as lib]
+            [discuss.references.lib :as rlib]
+            [discuss.translations :refer [translate] :rename {translate t}]))
 
 (defn fa-icon
   "Wrapper for font-awesome icons."
@@ -74,3 +77,42 @@
   [heading]
   (dom/h4 #js {:className "text-center"
                :style #js {:paddingBottom "0.3em"}} heading))
+
+(defn- remove-selection-then-reference!
+  "Remove selection on first click, then the reference if available."
+  []
+  (let [selection (lib/get-selection)
+        sel-ref (rlib/get-selected-reference)]
+    (cond
+      selection (lib/remove-selection!)
+      sel-ref (rlib/remove-selected-reference!))))
+
+(defn show-selection
+  "Shows selected text from website if available."
+  []
+  (let [selection (or (lib/get-selection) (:text (rlib/get-selected-reference)) "")]
+    (html
+     (if (> (count selection) 1)
+       [:div.input-group
+        [:span.input-group-addon.input-group-addon-left
+         (fa-icon "fa-quote-left")]
+        [:input.form-control {:style {:backgroundColor "rgb(250,250,250)"}
+                              :value selection
+                              :title (t :references :disabled/tooltip)
+                              :disabled true}]
+        [:span.input-group-addon
+         (fa-icon "fa-quote-right")]
+        [:span.input-group-addon.pointer {:onClick remove-selection-then-reference!}
+         (fa-icon "fa-times")]]
+       [:div.text-center {:style {:paddingBottom "1em"}}
+        (t :references :ask-to-add)]))))
+
+(defn remaining-characters
+  "Show remaining characters needed to submit a post."
+  [statement selected-search-results]
+  (if (nil? selected-search-results)
+    (let [remaining (- 10 (count statement))]
+      (if (pos? remaining)
+        (str remaining " " (t :common :chars-remaining))
+        (t :discussion :submit)))
+    (t :discussion :submit)))
--- src/discuss/views/add.cljs
+++ src/discuss/views/add.cljs
@@ -1,55 +1,13 @@
 (ns discuss.views.add
-  (:require [om.dom :as dom :include-macros true]
-            [om.next :as om :refer-macros [defui]]
+  (:require [om.next :as om :refer-macros [defui]]
             [sablono.core :as html :refer-macros [html]]
             [discuss.components.clipboard :as clipboard]
             [discuss.communication.main :as com]
-            [discuss.references.lib :as rlib]
             [discuss.translations :refer [translate] :rename {translate t}]
-            [discuss.utils.common :as lib]
             [discuss.utils.views :as vlib]
             [discuss.views.alerts :as valerts]
             [discuss.components.bubbles :as bubbles]
             [discuss.components.search.statements :as search]))
-
-(defn- remaining-characters
-  "Show remaining characters needed to submit a post."
-  [statement selected-search-results]
-  (if (nil? selected-search-results)
-    (let [remaining (- 10 (count statement))]
-      (if (pos? remaining)
-        (str remaining " " (t :common :chars-remaining))
-        (t :discussion :submit)))
-    (t :discussion :submit)))
-
-(defn- remove-selection-then-reference!
-  "Remove selection on first click, then the reference if available."
-  []
-  (let [selection (lib/get-selection)
-        sel-ref (rlib/get-selected-reference)]
-    (cond
-      selection (lib/remove-selection!)
-      sel-ref (rlib/remove-selected-reference!))))
-
-(defn- show-selection
-  "Shows selected text from website if available."
-  []
-  (let [selection (or (lib/get-selection) (:text (rlib/get-selected-reference)) "")]
-    (html
-     (if (> (count selection) 1)
-       [:div.input-group
-        [:span.input-group-addon.input-group-addon-left
-         (vlib/fa-icon "fa-quote-left")]
-        [:input.form-control {:style {:backgroundColor "rgb(250,250,250)"}
-                              :value selection
-                              :title (t :references :disabled/tooltip)
-                              :disabled true}]
-        [:span.input-group-addon
-         (vlib/fa-icon "fa-quote-right")]
-        [:span.input-group-addon.pointer {:onClick remove-selection-then-reference!}
-         (vlib/fa-icon "fa-times")]]
-       [:div.text-center {:style {:paddingBottom "1em"}}
-        (t :references :ask-to-add)]))))
 
 (defui StatementForm
   "Form to add a new statement to the discussion."
@@ -82,13 +40,13 @@
                   [:span.input-group-addon.pointer {:onClick search/remove-selected-search-result!}
                    (vlib/fa-icon "fa-times")])]
 
-               (show-selection)
+               (vlib/show-selection)
                [:button.btn.btn-default
                 {:onClick #(com/post-statement {:statement statement
                                                 :reference current-selection
                                                 :search/selected selected-search})
                  :disabled (when (nil? selected-search) (> 10 (count statement)))}
-                (remaining-characters statement selected-search)]]])))
+                (vlib/remaining-characters statement selected-search)]]])))
   (componentDidMount [this] (search/remove-all-search-related-results-and-selections)))
 (def statement-form (om/factory StatementForm))
 
@@ -127,13 +85,13 @@
                        [:span.input-group-addon.pointer {:onClick search/remove-selected-search-result!}
                         (vlib/fa-icon "fa-times")])]
 
-                    (show-selection)
+                    (vlib/show-selection)
                     [:button.btn.btn-default
                      {:onClick #(com/post-position {:position position
                                                     :reason reason
                                                     :reference current-selection
                                                     :search/selected selected-search})
                       :disabled (when (nil? selected-search) (> 10 (count smaller-input)))}
-                     (remaining-characters smaller-input selected-search)]]])))
+                     (vlib/remaining-characters smaller-input selected-search)]]])))
   (componentDidMount [this] (search/remove-all-search-related-results-and-selections)))
 (def position-form (om/factory PositionForm))
