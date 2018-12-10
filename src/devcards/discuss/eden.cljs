c3d0c255043c1cdcae98aeaab27f21f7e5822eeb
Christian Meter <christian.meter@uni-duesseldorf.de>
2018-12-10 14:51:42 +0100

    Add devcards for eden

--- src/devcards/discuss/eden.cljs
+++ src/devcards/discuss/eden.cljs
@@ -1,6 +1,34 @@
 (ns devcards.discuss.eden
   (:require [devcards.core :as dc :refer-macros [defcard defcard-om-next om-next-root]]
-            [discuss.eden.views :as eviews]))
+            [sablono.core :as html :refer-macros [html]]
+            [discuss.eden.views :as eviews]
+            [discuss.components.options :as options]
+            [discuss.parser :as parser]
+            [discuss.views :as views]
+            [discuss.communication.auth :as auth]
+            [discuss.utils.common :as lib]
+            [devcards.discuss.utils :as utils]))
 
-(defcard add-new-eden-argument
+(defcard-om-next connection-browser
+  options/ConnectionBrowser
+  parser/reconciler)
+
+(defcard buttons
+  (html [:div
+         (utils/button #(auth/login "Christian" "iamgroot") "Login as Christian")
+         (utils/button #(lib/last-api! "/town-has-to-cut-spending/justify/37/agree?history=/attitude/37") "Set :api/last-url")]))
+
+(defcard-om-next new-eden-argument
+  eviews/EDENArgumentForm
+  parser/reconciler)
+
+(defcard-om-next overview-menu
+  eviews/OverviewMenu
+  parser/reconciler)
+
+#_(defcard add-new-eden-argument
   (eviews/eden-argument-form {:click-fn (fn [] (println "clicked"))}))
+
+(defcard-om-next main-view
+  views/MainView
+  parser/reconciler)
