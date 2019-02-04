63309f756fc6db4a56b202b4baeff74f1e1e1db2
Christian Meter <christian.meter@uni-duesseldorf.de>
2019-02-04 22:01:46 +0100

    Bump version 0.5.1 --> 0.6.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject discuss "0.5.1"
+(defproject discuss "0.6.0"
   :description "Embedding dialog-based discussions into arbitrary web-contexts"
   :url "https://discuss.cs.uni-duesseldorf.de"
   :license {:name "MIT"
