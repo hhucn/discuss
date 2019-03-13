a2dff850046fb42e7aedea57b8fea040fb0ff2c7
Christian Meter <christian.meter@uni-duesseldorf.de>
2019-03-13 12:37:17 +0100

    Keep reference, clean up functions and make some private

--- src/discuss/communication/lib.cljs
+++ src/discuss/communication/lib.cljs
@@ -112,7 +112,6 @@
   "Get prepared statements and simply fire a POST request."
   ([request-url body handler error-handler headers]
    (log/info (str "Posting " (dissoc body :password) " to " request-url))
-   (lib/save-selection! nil)
    (POST request-url
          {:body            (lib/clj->json body)
           :handler         handler
