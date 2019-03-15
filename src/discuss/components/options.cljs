dc1fd41ec5356fd4d3eac6c6229510134b401bb4
Christian Meter <christian.meter@uni-duesseldorf.de>
2019-03-15 15:05:04 +0100

    Extend option to show connectivity to external services

--- src/discuss/components/options.cljs
+++ src/discuss/components/options.cljs
@@ -2,12 +2,15 @@
   (:require [om.next :as om :refer-macros [defui]]
             [cljs.spec.alpha :as s]
             [sablono.core :as html :refer-macros [html]]
+            [goog.string :refer [format]]
+            [goog.string.format]
             [discuss.utils.bootstrap :as bs]
             [discuss.translations :as translations :refer [translate] :rename {translate t}]
             [discuss.communication.auth :as auth]
             [discuss.utils.common :as lib]
             [discuss.utils.views :as vlib]
             [discuss.communication.lib :as comlib]
+            [discuss.communication.connectivity :as comcon]
             [discuss.config :as config]))
 
 (defn- language-button
@@ -19,7 +22,7 @@
 ;; -----------------------------------------------------------------------------
 ;; Set hosts
 
-(defn- set-host-config [this title current-host default-host set-host reset-host]
+(defn- set-host-config [this title current-host default-host set-host reset-host delete-host]
   (let [content (or (get (om/get-state this) :content) "")]
     [:div
      [:h5 title]
@@ -36,12 +39,16 @@
       [:span.input-group-addon (t :options :default)]
       [:input.form-control {:value (or default-host "")
                             :disabled true}]]
-     [:button.btn.btn-sm.btn-default {:onClick #(set-host content)
-                                      :disabled (empty? content)}
+     [:button.btn.btn-sm.btn-default {:onClick #(set-host content)}
       (t :options :save) " " title]
-     [:button.btn.btn-sm.btn-warning.pull-right
-      {:onClick reset-host}
-      (t :options :reset)]]))
+     [:div.pull-right
+      [:button.btn.btn-sm.btn-warning
+       {:onClick delete-host
+        :style {:marginRight "0.5em"}}
+       (t :options :delete)]
+      [:button.btn.btn-sm.btn-warning
+       {:onClick reset-host}
+       (t :options :reset)]]]))
 
 (s/fdef set-host-config
   :args (s/cat :this any? :title string? :current-host string?
@@ -54,7 +61,7 @@
   Object
   (render [this]
           (html
-           (set-host-config this "D-BAS API" lib/host-dbas config/remote-host lib/host-dbas! lib/host-dbas-reset!))))
+           (set-host-config this "D-BAS API" lib/host-dbas config/remote-host lib/host-dbas! lib/host-dbas-reset! #(lib/host-dbas! nil)))))
 (def host-dbas (om/factory HostDBAS))
 
 (defui HostEDEN
@@ -64,22 +71,41 @@
   Object
   (render [this]
           (html
-           (set-host-config this "EDEN Search" lib/host-eden config/search-host lib/host-eden! lib/host-eden-reset!))))
+           (set-host-config this "EDEN Search" lib/host-eden config/search-host lib/host-eden! lib/host-eden-reset! #(lib/host-eden! nil)))))
 (def host-eden (om/factory HostEDEN))
 
 
 ;; -----------------------------------------------------------------------------
 ;; Connectivity Information
 
+(defn- connection-icon [status service host]
+  (let [[class icon msg]
+        (cond
+          (empty? host)   ["" "fa-minus-square-o" "not configured"]
+          (true? status)  ["text-success" "fa-circle" "connected"]
+          (false? status) ["text-danger" "fa-circle-o" "disconnected"]
+          :default        ["text-warning" "fa-dot-circle-o" "connecting..."])]
+    [:div
+     [:span {:className class
+             :style {:padding-right "0.5em"}} (vlib/fa-icon icon)]
+     [:span (format "%s %s" service msg)]]))
+
 (defui ConnectivityStatus
   static om/IQuery
   (query [this]
-         `[:host/dbas-is-up? :host/eden-is-up?])
+         [:host/dbas-is-up? :host/eden-is-up?
+          :host/dbas :host/eden])
   Object
   (render [this]
-          (html [:div
-                 [:h5 "Status"
-                  ]])))
+          (let [{:keys [host/dbas-is-up? host/eden-is-up? host/dbas host/eden]} (om/props this)]
+            (html [:div
+                   [:h5 "Status"]
+                   (connection-icon dbas-is-up? "D-BAS" dbas)
+                   (connection-icon eden-is-up? "EDEN" eden)
+                   [:br]
+                   [:button.btn.btn-sm.btn-default {:onClick comcon/check-connectivity-of-hosts}
+                    "Neu verbinden"]]))))
+(def connectivity-status (om/factory ConnectivityStatus))
 
 
 
@@ -87,7 +113,7 @@
 ;; Demo Settings
 
 (defn- build-connections [{:keys [name dbas eden]}]
-  [[:button.btn.btn-default
+  [[:button.btn.btn-sm.btn-default
     {:key (lib/get-unique-key)
      :onClick (fn [_]
                 (lib/store-multiple-values-to-app-state!
@@ -109,9 +135,9 @@
              [:div.text-center
               (map build-connections config/demo-servers)
               [:br][:br]
-              [:button.btn.btn-default {:onClick #(lib/save-current-and-change-view! :options)}
+              [:button.btn.btn-sm.btn-default {:onClick #(lib/save-current-and-change-view! :options)}
                "Custom Settings"] " "
-              [:button.btn.btn-default {:onClick #(auth/login "Walter" "iamatestuser2016")}
+              [:button.btn.btn-sm.btn-default {:onClick #(auth/login "Walter" "iamatestuser2016")}
                "Login as Walter"]]))))
 (def connection-browser (om/factory ConnectionBrowser))
 
@@ -137,6 +163,10 @@
             [:hr]
             [:h4.text-center "Connection Browser"]
             (connection-browser (om/props this))
+            [:br]
+            [:hr]
+            [:h4.text-center "Connectivity"]
+            (connectivity-status (om/props this))
             [:br]
             [:hr]
             [:h4.text-center (t :options :routes)]
