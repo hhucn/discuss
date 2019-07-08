9c7afc45ad0d13c298e1fab2c70531009001fcda
Christian Meter <christian.meter@uni-duesseldorf.de>
2019-07-08 15:42:18 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -6,17 +6,17 @@
 
   :min-lein-version "2.5.3"
 
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.520"]
-                 [org.clojure/core.async "0.4.490" :exclusions [org.clojure/tools.reader]]
+                 [org.clojure/core.async "0.4.500" :exclusions [org.clojure/tools.reader]]
                  [org.clojure/test.check "0.9.0"]
                  [org.clojure/tools.reader "1.3.2"]
-                 [cljsjs/react "16.8.3-0"]
-                 [cljsjs/react-dom "16.8.3-0"]
+                 [cljsjs/react "16.8.6-0"]
+                 [cljsjs/react-dom "16.8.6-0"]
                  [cljsjs/create-react-class "15.6.3-1"]
                  [org.omcljs/om "1.0.0-beta4"]
                  [com.cognitect/transit-cljs "0.8.256"]
-                 [com.bhauman/figwheel-main "0.2.0"]
+                 [com.bhauman/figwheel-main "0.2.1"]
                  [com.bhauman/rebel-readline-cljs "0.1.4"]
                  [com.bhauman/cljs-test-display "0.1.1"]
                  [com.velisco/strgen "0.1.8"]
@@ -25,7 +25,7 @@
                  [spec-provider "0.4.14"]
                  [cljs-ajax "0.8.0"]
                  [devcards "0.2.6"]
-                 [sablono "0.8.5"]
+                 [sablono "0.8.6"]
                  [inflections "0.13.2"]]
 
   :plugins [[lein-ancient "0.6.10"]
@@ -46,7 +46,7 @@
 
   :profiles {:dev {:dependencies [[binaryage/devtools "0.9.10"]
                                   [nrepl "0.6.0"]
-                                  [cider/piggieback "0.4.0"]]
+                                  [cider/piggieback "0.4.1"]]
                    :source-paths ["src" "script" "test"]
                    :resource-paths ["target"]
                    :repl-options {:init (set! *print-length* 50)
