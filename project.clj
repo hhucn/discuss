2cfc0cdca0524a65bb4e649847b73dc17bae0a30
Christian Meter <cmeter@googlemail.com>
2020-01-27 17:39:00 +0100

    Bump version: 0.16.2 → 0.16.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject discuss "0.16.2"
+(defproject discuss "0.16.3"
   :description "Embedding dialog-based discussions into arbitrary web-contexts"
   :url "https://discuss.cs.uni-duesseldorf.de"
   :license {:name "MIT"
